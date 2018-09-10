891d49b07852c1d133d619644f45fc4ff86ebd80
Kandis Flick <kandisflick@gmail.com>
2018-09-10 03:16:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if string === "I love you, Grandma." {
+    return "I love you, too."
+  } else if string === logWhisper(string) {
+    return "I can't hear you!"
+  } else if string === logShout(string) {
+    return "YES INDEED!"
+  }
+}
